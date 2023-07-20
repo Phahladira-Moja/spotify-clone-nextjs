26e0cdb2077b8ca75ae997ae6015691baaa702fc
Phahladira-Moja <phahladira@patala.co.za>
2023-07-20 22:05:51 +0200

    Created the get songs action and the Song Item component
    - Created a hook to get the image public url
    - created the type for songs
    - created the play button component
    - created the Page content component for the songs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["hemndlyvkyhigeffrucp.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
