a9c1f45d59eaa32d2cbd56469fa3b39f1b629ab0
Phahladira-Moja <phahladira@patala.co.za>
2023-07-20 23:39:44 +0200

    Added the likeButton component
    - connected it with DB

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,4 +1,5 @@
 "use client";
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 import React from "react";
@@ -34,6 +35,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           </div>
 
           {/* TODO: Add Like Button Here */}
+          <LikeButton songId={song.id} />
         </div>
       ))}
     </div>
