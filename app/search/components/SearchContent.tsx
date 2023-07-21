8caa6766a81c20d6a0993bf0775def5ffeb7193e
Phahladira-Moja <phahladira@patala.co.za>
2023-07-22 01:13:05 +0200

    Added the play song functionality
    - added the functionality to go back , forward, pause, play and volume
    - added functions to add songs to playlist, also to play a single song
    - added onlick functions to library, liked and search for playing func
    - Added the player content components, and hooks

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
 import React from "react";
 
@@ -9,6 +10,8 @@ interface SearchContentProps {
 }
 
 const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return (
       <div
@@ -31,7 +34,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
       {songs.map((song) => (
         <div key={song.id} className="flex items-center gap-x-4 w-full">
           <div className="flex-1">
-            <MediaItem onClick={() => {}} data={song} />
+            <MediaItem onClick={(id: string) => onPlay(id)} data={song} />
           </div>
 
           {/* TODO: Add Like Button Here */}
---
+++ components/Player.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import usePlayer from "@/hooks/usePlayer";
+import React from "react";
+import PlayerContent from "./PlayerContent";
+
+const Player = () => {
+  const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
+
+  const songUrl = useLoadSongUrl(song!);
+
+  if (!song || !songUrl || !player.activeId) {
+    return null;
+  }
+
+  return (
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+      <PlayerContent
+        // Nb we need the key so that when we skip songs the component
+        // is completely destroyed so that a new one is created
+        key={songUrl}
+        song={song}
+        songUrl={songUrl}
+      />
+    </div>
+  );
+};
+
+export default Player;
